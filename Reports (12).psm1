c362f6e8eceef54699914e8167113289ac2b0cd4
Aaron Strasler <Aaron.M.Strasler@outlook.com>
2026-07-10 19:43:24 -0500

    Delete Reports (12).psm1
    duplicate.

--- Reports (12).psm1
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Aaron System Utility Launcher
-:: Launches ASU.ps1 with required execution policy and bypass
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0ASU.ps1" %*
-pause
